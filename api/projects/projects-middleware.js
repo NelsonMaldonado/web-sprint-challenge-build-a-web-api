44a27b0935687d4177737755bda8edf340e6bf4b
Nelson Maldonado <play2nino@gmail.com>
2022-01-14 13:25:02 -0800

    wip

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -5,6 +5,19 @@ function handleError(err, req, res, next) {
   res.status(err.status || 500).json({ message: err.message })
 }
 
+function checkProjectId(req, res, next) {
+  Projects.get(req.params.id)
+    .then((project) => {
+      if (project) {
+        req.projectFromDb = project
+        next()
+      } else {
+        next({ status: 404, message: "no project found" })
+      }
+    })
+    .catch(next)
+}
+
 module.exports = {
   handleError,
 }
