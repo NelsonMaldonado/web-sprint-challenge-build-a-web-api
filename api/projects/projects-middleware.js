1172e86148c94ba29c6301fcb3493515c6fbc293
Nelson Maldonado <play2nino@gmail.com>
2022-01-14 13:20:53 -0800

    wip

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,10 @@
 // add middlewares here related to projects
+const Projects = require("./projects-model")
+
+function handleError(err, req, res, next) {
+  res.status(err.status || 500).json({ message: err.message })
+}
+
+module.exports = {
+  handleError,
+}
