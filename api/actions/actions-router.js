cc79a1255f07052fe6a8dcf9a5648c32079e3206
Nelson Maldonado <play2nino@gmail.com>
2022-01-14 14:32:24 -0800

    wip

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,11 +1,21 @@
 // Write your "actions" router here!
 const express = require("express")
-const { handleError } = require("./actions-middlware")
+const res = require("express/lib/response")
+const { handleError, checkActionId } = require("./actions-middlware")
 const Actions = require("./actions-model")
 const router = express.Router()
 
-route.get("/", (req, res, next) => {
-  res.send("hello from actions enpoint")
+router.get("/", (req, res, next) => {
+  Actions.get(req.params.id)
+    .then((action) => {
+      res.status(200).json(action)
+    })
+    .catch(next)
 })
 
+router.get("/:id", checkActionId, (req, res) => {
+  res.status(200).json(req.projectFromDb)
+})
+
+router.use(handleError)
 module.exports = router
