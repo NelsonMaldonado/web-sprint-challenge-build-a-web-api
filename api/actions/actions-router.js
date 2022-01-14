965dc158ef5a4d98516be490a5d3ca1f53840698
Nelson Maldonado <play2nino@gmail.com>
2022-01-14 14:16:50 -0800

    wip

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,11 @@
 // Write your "actions" router here!
+const express = require("express")
+const { handleError } = require("./actions-middlware")
+const Actions = require("./actions-model")
+const router = express.Router()
+
+route.get("/", (req, res, next) => {
+  res.send("hello from actions enpoint")
+})
+
+module.exports = router
