25ab3da2ae658bd2ad48f0dbd8e847de17ae11fb
Nelson Maldonado <play2nino@gmail.com>
2022-01-14 14:32:16 -0800

    wip

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -5,6 +5,20 @@ function handleError(err, req, res, next) {
   res.status(err.status || 500).json({ message: err.message })
 }
 
+function checkActionId(req, res, next) {
+  Actions.get(req.params.id)
+    .then((action) => {
+      if (action) {
+        req.actionFromDb = action
+        next()
+      } else {
+        next({ status: 404, message: "no projects found" })
+      }
+    })
+    .catch(next)
+}
+
 module.exports = {
   handleError,
+  checkActionId,
 }
