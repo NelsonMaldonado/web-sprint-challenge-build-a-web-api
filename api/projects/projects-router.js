a8b9775a1da81ca97de95122dbca0be92704ef49
Nelson Maldonado <play2nino@gmail.com>
2022-01-14 13:58:52 -0800

    wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,7 +1,12 @@
 // Write your "projects" router here!
 const express = require("express")
+const {
+  handleError,
+  checkProjectId,
+  validateCompleted,
+  validateProject,
+} = require("./projects-middleware")
 const Projects = require("./projects-model")
-
 const router = express.Router()
 
 router.get("/", (req, res, next) => {
@@ -11,5 +16,51 @@ router.get("/", (req, res, next) => {
     })
     .catch(next)
 })
+
+router.get("/:id", checkProjectId, (req, res) => {
+  res.status(200).json(req.projectFromDb)
+})
+
+router.post("/", validateProject, (req, res, next) => {
+  Projects.insert(req.body)
+    .then((project) => {
+      res.status(201).json(project)
+    })
+    .catch(next)
+})
+
+router.put(
+  "/:id",
+  checkProjectId,
+  validateProject,
+  validateCompleted,
+  (req, res, next) => {
+    Projects.update(req.params.id, req.body)
+      .then((updatedProject) => {
+        res.status(200).json(updatedProject)
+      })
+      .catch(next)
+  }
+)
+
+router.delete("/:id", checkProjectId, (req, res, next) => {
+  Projects.remove(req.params.id)
+    .then(() => {
+      res.status(200).json({
+        message: "Project Removed",
+      })
+    })
+    .catch(next)
+})
+
+router.get("/:id/actions", checkProjectId, (req, res, next) => {
+  Projects.getProjectActions(req.params.id)
+    .then((actions) => {
+      res.status(200).json(actions)
+    })
+    .catch(next)
+})
+
+router.use(handleError)
 
 module.exports = router
