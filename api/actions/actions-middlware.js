b4b557e5b8441a37a65a6a4fa435d73fe7db46db
Nelson Maldonado <play2nino@gmail.com>
2022-01-14 16:21:07 -0800

    action router working

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,8 +1,9 @@
 // add middlewares here related to actions
+const { getProjectActions } = require("../projects/projects-model")
 const Actions = require("./actions-model")
 
 function handleError(err, req, res, next) {
-  res.status(err.status || 500).json({ message: err.message })
+  res.status(err.status || 400).json({ message: err.message })
 }
 
 function checkActionId(req, res, next) {
