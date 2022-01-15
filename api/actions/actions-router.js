dad6654191841546f8ef6a242b4e96799c6abff4
Nelson Maldonado <play2nino@gmail.com>
2022-01-14 16:21:15 -0800

    wip

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -25,5 +25,23 @@ router.post("/", (req, res, next) => {
     .catch(next)
 })
 
+router.put("/:id", checkActionId, (req, res, next) => {
+  Actions.update(req.params.id, req.body)
+    .then((update) => {
+      res.status(200).json(update)
+    })
+    .catch(next)
+})
+
+router.delete("/:id", checkActionId, (req, res, next) => {
+  Actions.remove(req.params.id)
+    .then(() => {
+      res.status(200).json({
+        message: "Action removed",
+      })
+    })
+    .catch(next)
+})
+
 router.use(handleError)
 module.exports = router
